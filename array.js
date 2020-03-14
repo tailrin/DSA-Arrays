0d47c533930daf78283e3f150a8ac45e673c39f6
LZylstra <lindseyzylstra@gmail.com>
2020-03-14 11:46:34 -0700

    added insert

--- array.js
+++ array.js
@@ -34,13 +34,25 @@ class Array {
     return memory.get(this.ptr + index);
   }
 
-  pop(){
-      if(this.length == 0){
-          throw new Error('Index error')
-      }
-      const value = memory.get(this.ptr + this.length -1);
-      this.length --;
-      return value;
+  pop() {
+    if (this.length == 0) {
+      throw new Error("Index error");
+    }
+    const value = memory.get(this.ptr + this.length - 1);
+    this.length--;
+    return value;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index >= this.length) {
+      throw new Error("Index error");
+    }
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO);
+    }
+    memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+    memory.set(this.ptr + index, value);
+    this.length++;
   }
 }
 
