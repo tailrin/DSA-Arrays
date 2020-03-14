b7a1d989ca0b9435b0005875649863fc43733b24
LZylstra <lindseyzylstra@gmail.com>
2020-03-14 12:24:15 -0700

    Answered question 2

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const Array = require("./array");
 
-
 function main() {
   Array.SIZE_RATIO = 3;
 
@@ -8,9 +7,15 @@ function main() {
   let arr = new Array();
 
   // Add an item to the array
-  arr.push(3);
+  arr.push(3); //length: 1, capacity: 3, ptr: 0
 
-  //length: 1, capacity: 3
+  arr.push(5);
+  arr.push(15);
+  arr.push(19);
+  arr.push(45);
+  arr.push(10);
+  // length: 6, capacity: 12, ptr: 3
+  // Only resizes if it needs to when it got to 4 it resizes.
 
   console.log(arr);
 }
