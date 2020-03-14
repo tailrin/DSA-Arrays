06ca58f06a28758ebefb6cc6c4a1b415093131bb
joshua hahn <tailrin.jh@gmail.com>
2020-03-14 12:43:23 -0600

    added pop function

--- array.js
+++ array.js
@@ -33,6 +33,15 @@ class Array {
     }
     return memory.get(this.ptr + index);
   }
+
+  pop(){
+      if(this.length == 0){
+          throw new Error('Index error')
+      }
+      const value = memory.get(this.ptr + this.length -1);
+      this.length --;
+      return value;
+  }
 }
 
 Array.SIZE_RATIO = 3;
