97c807382a22b5704cf0ee163bc415ddb7d29be3
LZylstra <lindseyzylstra@gmail.com>
2020-03-14 11:32:47 -0700

    added push

--- array.js
+++ array.js
@@ -1,9 +1,20 @@
-const memory = require('./memory');
+const memory = require("./memory");
 
 class Array {
-    constructor(){
-        this.length = 0;
-        this._capacity = 0;
-        this.ptr = memory.allocate(this.length);
+  constructor() {
+    this.length = 0;
+    this._capacity = 0;
+    this.ptr = memory.allocate(this.length);
+  }
+
+  push(value) {
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO);
     }
+
+    memory.set(this.ptr + this.length, value);
+    this.length++;
+  }
 }
+
+Array.SIZE_RATIO = 3;
